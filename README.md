0bc1cec1f8a83e15cfddbbf85faa812c195803ba
Wissam <wissamhisham@gmail.com>
2020-10-08 19:48:41 -0700

    updates

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Bulk Email Sender
-A simple program that sends a similar email (with attachments) to multiple recipients without showing all addresses the email was sent to.
+A simple program that sends an email (with attachments) to multiple recipients without showing all addresses the email was sent to.
 
-I originally created this to reach out to recruiters, but have repurposed the code to apply to more general use cases.
+I originally created this to reach out to multiple recruiters at once with my resume, but have repurposed the code to apply to more general use cases.
+
+## Installation
+```python
+pip install yagmail
+pip install keyring
+```
+
+## Usage
+Edit the variables in "emailer.py" to suit your needs, then run it.
--- src/emailer.py
+++ src/emailer.py
@@ -1,20 +1,34 @@
 import yagmail
 
-recipient_emails = input("Path to receiver list: ")
+# enter the email you are sending from
 sender_email = "testemail6213@gmail.com"
 
-body = [
-    "test message"
+# enter the emails you are sending to
+recipient_emails = [
+    "testemail6213@gmail.com"
 ]
 
-file1 = "test_file.txt"
+# enter the subject of the email
+title = "test title"
+
+# enter the body of the message
+body = [
+    "test message\n\n"
+    "test paragraph"
+]
+
+# enter the filepaths of the files you want to attach
+files = [
+    r"Bulk-Email-Sender\src\test\test_file.txt", 
+    r"Bulk-Email-Sender\src\test\test_image.jpg"
+]
 
 yag = yagmail.SMTP(sender_email)
 
 for r in recipient_emails:
     yag.send(
         to = r,
-        subject = "test title",
+        subject = title,
         contents = body,
-        attachments = file1
+        attachments = files
     )

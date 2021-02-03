2e831865e4462a6117b226d477c632ddc19f3a7f
Wissam <wissamhisham@gmail.com>
2021-02-02 22:03:08 -0800

    updates

--- src/emailer.py
+++ src/emailer.py
@@ -3,33 +3,27 @@ import yagmail
 # enter the Gmail you are sending from
 sender_email = "testemail6213@gmail.com"
 
-# enter the emails you are sending to
-recipient_emails = [
-    "testemail6213@gmail.com",
-	"testemail5354@gmail.com"
-]
-
-# enter the subject of the email
-title = "test title"
-
-# enter the body of the message
-body = [
-    "test message\n\n"
-    "test paragraph"
+# enter the names and emails you are sending to
+recipients = [
+    ('Name1', 'testemail6213@gmail.com', 'Position1')
+	('Name2', 'testemail5354@gmail.com', 'Position2')
 ]
 
 # enter the filepaths of the files you want to attach (must prefix with "r")
 files = [
-    r"Bulk-Email-Sender\src\test\test_file.txt", 
-    r"Bulk-Email-Sender\src\test\test_image.jpg"
+    r'Bulk-Email-Sender\src\test\test_file.txt', 
+    r'Bulk-Email-Sender\src\test\test_image.jpg'
 ]
 
 email = yagmail.SMTP(sender_email)
 
-for r in recipient_emails:
+# edit your email here
+for r in recipients:
     email.send(
-        to = r,
-        subject = title,
-        contents = body,
+        to = r[1],
+        # edit your title here
+        subject = f'Regarding position {r[2]}',
+        # edit your message here
+        contents = [f'Hello {r[0]},\n\n I am reaching out in regards to the position of {r[2]}. I hope you will consider me.\n\n Sincerely, first_name last_name'],
         attachments = files
     )
